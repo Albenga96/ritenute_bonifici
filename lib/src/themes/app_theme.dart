43811000d71df3b4323790bad3d36625c8765342
Albenga96 <albertoferroni.af@gmail.com>
2021-11-22 14:42:38 +0100

    Update app theme

--- lib/src/themes/app_theme.dart
+++ lib/src/themes/app_theme.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ritenute_bonifici/src/themes/ritenute_colors.dart';
 import 'package:ritenute_bonifici/src/themes/ritenute_text_sizes.dart';
 
 ThemeData ritenuteTheme() => ThemeData(
+      fontFamily: GoogleFonts.montserrat().fontFamily,
       primaryColor: RitenuteColors.ritenutePrimaryColor,
       scaffoldBackgroundColor: RitenuteColors.ritenuteBackgroundColor,
       textTheme: TextTheme(
         headline1: const TextStyle(
           fontSize: RitenuteTextSizes.textBig,
           color: RitenuteColors.ritenuteTextColor,
+          fontWeight: FontWeight.bold,
         ),
         headline2: TextStyle(
+          fontWeight: FontWeight.bold,
           fontSize: RitenuteTextSizes.textMedium,
           color: RitenuteColors.ritenuteTextColor.withOpacity(0.4),
         ),
         headline3: const TextStyle(
+          fontWeight: FontWeight.bold,
           color: RitenuteColors.ritenuteWhiteColor,
           fontSize: RitenuteTextSizes.textMedium,
         ),
         headline4: const TextStyle(
+          fontWeight: FontWeight.bold,
           color: RitenuteColors.ritenutePrimaryColor,
           fontSize: RitenuteTextSizes.textSmall,
         ),
         headline5: TextStyle(
+          fontWeight: FontWeight.bold,
           color: RitenuteColors.ritenuteTextColor.withOpacity(0.4),
           fontSize: RitenuteTextSizes.textSmall,
         ),
--- lib/src/themes/ritenute_colors.dart
+++ lib/src/themes/ritenute_colors.dart
@@ -4,7 +4,6 @@ class RitenuteColors {
   static const Color ritenutePrimaryColor = Color(0xFFFF8A00);
   static const Color ritenuteSecondaryColor = Color(0xFFC4C4C4);
   static const Color ritenuteTextColor = Color(0xFF000000);
-  static Color ritenuteBackgroundColor =
-      ritenuteSecondaryColor.withOpacity(0.2);
-  static const ritenuteWhiteColor = Color(0xFFFFFFFF);
+  static const Color ritenuteBackgroundColor = Color(0xFFE8E6E6);
+  static const Color ritenuteWhiteColor = Color(0xFFFFFFFF);
 }
